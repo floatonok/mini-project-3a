bdf40ca102a694a31e5172b984d7d8abe52a3bec
Isabella Ong <isabellaong@gmail.com>
2016-07-10 17:11:52 +0800

    adds NASA categories endpoint with random color generator for each type of event

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ $(document).ready(function () {
   // DRAWING THE MAP
   var mymap = L.map('mapid').setView([0, 0], 2)
 
-  L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-    maxZoom: 18
+  L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    maxZoom: 19
   }).addTo(mymap)
 
   // L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZmxvYXRvbm9rIiwiYSI6ImNpcWM2eGk2ZjAxbXdmb25wZHliOTVyczkifQ.0EhN9Rbwy2iPWLH1GeHdkw', {
@@ -42,12 +42,13 @@ $(document).ready(function () {
   // ------------- GEOMETRY TYPE: POINT -------------
   var geojsonMarkerOptions = {
     radius: 8,
-    fillColor: '#ff7800',
     color: '#000',
     weight: 1,
     opacity: 1,
-    fillOpacity: 0.8
+    fillOpacity: 0.8,
+    fillColor: markerColor
   }
+
   // var states2 = [{
   //   'type': 'Feature',
   //   'geometry': {
@@ -64,6 +65,21 @@ $(document).ready(function () {
   // }).addTo(mymap)
 
   // ------------- AJAX REQUEST -------------
+  var allColors = []
+  var eventColor = {id: null, color: null}
+  var markerColor
+
+  $.ajax({
+    type: 'GET',
+    url: 'http://eonet.sci.gsfc.nasa.gov/api/v2.1/categories',
+    dataType: 'json'
+  }).done(function (data) {
+    data.categories.forEach(function (datum) {
+      eventColor.id = datum.id
+      eventColor.color = Math.floor(Math.random() * 16777215).toString(16)
+      allColors.push(eventColor)
+    })
+  })
 
   $.ajax({
     type: 'GET',
@@ -71,17 +87,32 @@ $(document).ready(function () {
     dataType: 'json'
   }).done(function (data) {
     data.events.forEach(function (datum) {
+      allColors.forEach(function (color) {
+        if (datum.categories[0].id === color.id) {
+          console.log(color.color)
+          markerColor = color.color
+        }
+      })
       var geometry = datum.geometries
       var circle = L.geoJson(geometry, {
         pointToLayer: function (feature, latlng) {
           return L.circleMarker(latlng, geojsonMarkerOptions)
         }
       }).addTo(mymap)
-      var date = geometry[0].date
-      circle.bindPopup(datum.title + '<br>' + new Date(date).toDateString())
-      // circle.onclick = function () {
-      //   $('#date').append('THE DATE GOES HERE!')
-      // }
+      var date = new Date(geometry[0].date).toDateString()
+      circle.bindPopup(datum.title + '<br>' + date)
+      circle.addEventListener('click', function (e) {
+        e = e || window.event
+        $('#title').empty()
+        $('#date').empty()
+        $('#description').empty()
+        $('#source').empty()
+
+        $('#title').append(datum.title)
+        $('#date').append('CATEGORY: ' + datum.categories[0].title + ' | ' + date)
+        $('#description').append(datum.description)
+        $('#source').append('<a href="' + datum.sources[0].url + '">[Source]</a>')
+      })
     })
   })
 })
