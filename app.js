5bda4d9f5bc2eb32f7b072cc90af0f99a15bd08c
Isabella Ong <isabellaong@gmail.com>
2016-07-10 18:59:35 +0800

    adds unique color for each cateogory of event on the map with corresponding legend

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 /* globals L $ */
 $(document).ready(function () {
   // DRAWING THE MAP
+
   var mymap = L.map('mapid').setView([0, 0], 2)
 
   L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -8,66 +9,22 @@ $(document).ready(function () {
     maxZoom: 19
   }).addTo(mymap)
 
-  // L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZmxvYXRvbm9rIiwiYSI6ImNpcWM2eGk2ZjAxbXdmb25wZHliOTVyczkifQ.0EhN9Rbwy2iPWLH1GeHdkw', {
-  //   attribution: 'Map data &copy; <a href='http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-  //   maxZoom: 18,
-  //   accessToken: 'pk.eyJ1IjoiZmxvYXRvbm9rIiwiYSI6ImNpcWM2eml6bDAxb3Vmcm0xdnQ0ZzBoazMifQ.e712oBJUID9CAIrO1PmPbw'
-  // }).addTo(mymap)
+  // AJAX REQUEST
 
-  // ------------- GEOMETRY TYPE: POLYGON -------------
-  // var states = [{
-  //   'type': 'Feature',
-  //   'properties': {'party': 'Republican'},
-  //   'geometry': {
-  //     'type': 'Polygon',
-  //     'coordinates': [[
-  //             [99.60, 51.05],
-  //             [99.609375, 61.495014243336605],
-  //             [113.984375, 61.495014243336605],
-  //             [113.984375, 51.05119274780004],
-  //             [99.609375, 51.05119274780004]
-  //     ]]
-  //   }
-  // }]
-  //
-  // L.geoJson(states, {
-  //   style: function (feature) {
-  //     switch (feature.properties.party) {
-  //       case 'Republican': return {color: '#ff0000'}
-  //       case 'Democrat': return {color: '#0000ff'}
-  //     }
-  //   }
-  // }).addTo(mymap)
-
-  // ------------- GEOMETRY TYPE: POINT -------------
-  var geojsonMarkerOptions = {
-    radius: 8,
-    color: '#000',
-    weight: 1,
-    opacity: 1,
-    fillOpacity: 0.8,
-    fillColor: markerColor
-  }
-
-  // var states2 = [{
-  //   'type': 'Feature',
-  //   'geometry': {
-  //     'type': 'Point',
-  //     'coordinates':
-  //             [-120.85361111111, 38.969166666667]
-  //   }
-  // }]
-
-  // L.geoJson(states2, {
-  //   pointToLayer: function (feature, latlng) {
-  //     return L.circleMarker(latlng, geojsonMarkerOptions)
-  //   }
-  // }).addTo(mymap)
-
-  // ------------- AJAX REQUEST -------------
   var allColors = []
-  var eventColor = {id: null, color: null}
+  // var eventColor = {id: null, color: null}
   var markerColor
+
+  function invertColor (hexTripletColor) {
+    var color = hexTripletColor
+    color = color.substring(1)           // remove #
+    color = parseInt(color, 16)          // convert to integer
+    color = 0xFFFFFF ^ color             // invert three bytes
+    color = color.toString(16)           // convert to hex
+    color = ('000000' + color).slice(-6) // pad with leading zeros
+    color = '#' + color                  // prepend #
+    return color
+  }
 
   $.ajax({
     type: 'GET',
@@ -75,9 +32,11 @@ $(document).ready(function () {
     dataType: 'json'
   }).done(function (data) {
     data.categories.forEach(function (datum) {
-      eventColor.id = datum.id
-      eventColor.color = Math.floor(Math.random() * 16777215).toString(16)
-      allColors.push(eventColor)
+      allColors.push({id: datum.id, title: datum.title, color: '#' + Math.floor(Math.random() * 16777215).toString(16)})
+    })
+    allColors.forEach(function (col) {
+      var complement = invertColor(col.color)
+      $('#list').append('<li>' + '<div style="background-color:' + col.color + '; color:' + complement + '; width: 100px; height: 35px;">' + col.title + '</div>' + '</li>')
     })
   })
 
@@ -89,16 +48,33 @@ $(document).ready(function () {
     data.events.forEach(function (datum) {
       allColors.forEach(function (color) {
         if (datum.categories[0].id === color.id) {
-          console.log(color.color)
           markerColor = color.color
+          console.log(markerColor)
         }
       })
       var geometry = datum.geometries
-      var circle = L.geoJson(geometry, {
-        pointToLayer: function (feature, latlng) {
-          return L.circleMarker(latlng, geojsonMarkerOptions)
-        }
-      }).addTo(mymap)
+      if (geometry[0].type === 'Point') {
+        var circle = L.geoJson(geometry, {
+          pointToLayer: function (feature, latlng) {
+            return L.circleMarker(latlng, {
+              radius: 15,
+              color: 'markerColor',
+              weight: 1,
+              opacity: 1,
+              fillOpacity: 0.4,
+              fillColor: markerColor
+            })
+          }
+        }).addTo(mymap)
+      } else {
+        circle = L.geoJson(geometry, {
+          style: {
+            'color': markerColor,
+            'weight': 4,
+            'opacity': 0.4
+          }
+        }).addTo(mymap)
+      }
       var date = new Date(geometry[0].date).toDateString()
       circle.bindPopup(datum.title + '<br>' + date)
       circle.addEventListener('click', function (e) {
